cf6404b80bb80926e937122de6846f54b6d0e0e0
Daniel Lovasko <daniel.lovasko@gmail.com>
2019-10-13 00:32:12 +0000

    Legal headers.

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Copyright (c) 2019 Daniel Lovasko
+# All Rights Reserved
+#
+# Distributed under the terms of the 2-clause BSD License. The full
+# license is in the file LICENSE, distributed as part of this software.
 
 c99 -DAGG_DBL -o dbl_test test.c ../impl/agg.c -lm && ./dbl_test
 c99 -DAGG_FLT -o flt_test test.c ../impl/agg.c -lm && ./flt_test
