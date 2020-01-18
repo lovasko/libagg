9ddf8879ceabd9667a91886ab63689a5b8e1b4b6
Daniel Lovasko <daniel.lovasko@gmail.com>
2020-01-18 17:23:47 +0000

    Use of generic type name in error thresholds.

--- test/err.h
+++ test/err.h
@@ -13,7 +13,7 @@
 // iteration.
 #if AGG_BIT == 32
   #ifdef __FAST_MATH__
-    static const float err[13][6] = {
+    static const AGG_TYPE err[13][6] = {
       {0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f},  // fst
       {0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f},  // lst
       {0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f},  // cnt
@@ -29,7 +29,7 @@
       {1.0e1f,  1.0e0f,  1.0e0f,  1.0e-1f, 1.0e-1f, 1.0e-1f}, // med
     };
   #else
-    static const float err[13][6] = {
+    static const AGG_TYPE err[13][6] = {
       {0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f},  // fst
       {0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f},  // lst
       {0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f,  0.0e0f},  // cnt
@@ -47,7 +47,7 @@
   #endif
 #elif AGG_BIT == 64
   #ifdef __FAST_MATH__
-    static const double err[13][6] = {
+    static const AGG_TYPE err[13][6] = {
       {0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0},   // fst
       {0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0},   // lst
       {0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0},   // cnt
@@ -63,7 +63,7 @@
       {1.0e1,   1.0e0,   1.0e0,   1.0e-1,  1.0e-2,  1.0e-3},  // med
     };
   #else
-    static const double err[13][6] = {
+    static const AGG_TYPE err[13][6] = {
       {0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0},   // fst
       {0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0},   // lst
       {0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0,   0.0e0},   // cnt
