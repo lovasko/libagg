a47f3e9c97189cb8ae9fb009b2e145913005a62d
Daniel Lovasko <daniel.lovasko@gmail.com>
2021-06-08 14:07:49 +0200

    Remove absolute success criteria.

--- cap/cap.sh
+++ cap/cap.sh
@@ -5,8 +5,6 @@
 # Distributed under the terms of the 2-clause BSD License. The full
 # license is in the file LICENSE, distributed as part of this software.
 
-set -e
-
 # Ensure that all relative paths remain correct.
 cd $(dirname $0)
 
